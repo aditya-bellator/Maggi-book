4be1c407e3560f98aa0550bdbdbd805324e805f8
aditya-bellator <aditya@bellatorgroup.in>
2024-07-19 10:09:27 +0530

    login

--- src/layout/navbar/index.jsx
+++ src/layout/navbar/index.jsx
@@ -1,17 +1,30 @@
 import  { useState } from 'react'
 import "./styled.scss"
 import { Link } from 'react-router-dom'
+import Login from "../../Pages/login/Login"
 
 const Navbar = () => {
   const [activeTab, setActiveTab] = useState(0)
+  const [isModalOpen, setModalOpen] = useState(false)
   const navArray = ["Lottery","SportsBook1","Exchange","Live Casino","Slots","Fantasy Game"]
+
+  const handlTabClick = (i)=>{
+    setActiveTab(i)
+    if(i===0){
+      setModalOpen(true)
+    }
+  };
+  const closeModal=()=>{
+    setModalOpen(false)
+  }
   return (
     <div className='nav-main'>
       <ul className='menu-list'>
-        {navArray?.map((item,i)=><Link to={""} key={item}><li className={i == activeTab ?`active`:""} onClick={()=>setActiveTab(i)}>{item}</li></Link>)}
+        {navArray?.map((item,i)=><Link to={""} key={item}><li className={i == activeTab ?`active`:""} onClick={()=>handlTabClick(i)}>{item}</li></Link>)}
         
     
       </ul>
+      <Login isOpen={isModalOpen}onClose={closeModal} />
     </div>
   )
 }
